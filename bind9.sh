f8ce2a5ea95455df851428565054a7170986c268
compact-orb <89509108+compact-orb@users.noreply.github.com>
2024-12-01 12:57:24 +0000

    Make output more clear

--- bind9.sh
+++ bind9.sh
@@ -6,11 +6,11 @@ set -euo pipefail
 IFS=$'\n\t'
 
 #Install required bundles
-echo 'Install required bundles'
+echo '——Install required bundles——'
 swupd bundle-add c-basic devpkg-libcap devpkg-libuv devpkg-nghttp2 devpkg-openssl devpkg-userspace-rcu git
 
 #Clean swupd cache
-echo 'Clean swupd cache'
+echo '——Clean swupd cache——'
 swupd clean --all
 
 #Build in /tmp
@@ -18,21 +18,21 @@ echo 'Build in /tmp'
 cd /tmp
 
 #Download bind9
-echo 'Download bind9'
+echo '——Download bind9——'
 curl --output Archive.tar.gz https://gitlab.isc.org/isc-projects/bind9/-/archive/v9.21.2/bind9-v9.21.2.tar.gz
 tar --extract --file=Archive.tar.gz
 rm Archive.tar.gz
 cd bind9-*
 
 #Configure bind9
-echo 'Configure bind9'
+echo '——Configure bind9——'
 autoreconf --install
 CC=clang CFLAGS='-O3 -flto=thin' LDFLAGS='-fuse-ld=lld' ./configure
 
 #Build bind9
-echo 'Build bind9'
+echo '——Build bind9——'
 make --jobs=$(nproc)
 
 #Install bind9
-echo 'Install bind9'
+echo '——Install bind9——'
 make install
