651cfd221f61f9ae87d63b25f85e38e84506fb9b
compact-orb <89509108+compact-orb@users.noreply.github.com>
2024-12-01 22:12:57 +0000

    Add mimalloc build script

---
+++ mimalloc.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/bash
+
+#Bash strict mode
+set -euo pipefail
+IFS=$'\n\t'
+
+#Install required bundles
+echo '——Install required bundles——'
+swupd bundle-add c-basic
+
+#Clean swupd cache
+echo '——Clean swupd cache——'
+swupd clean --all
+
+#Build in /tmp
+echo '——Build in /tmp——'
+cd /tmp
+
+#Download mimalloc
+echo '——Download mimalloc——'
+curl --output Archive.tar.gz --location https://github.com/microsoft/mimalloc/archive/refs/tags/v2.1.7.tar.gz
+tar --extract --file=Archive.tar.gz
+rm Archive.tar.gz
+cd mimalloc-*
+
+#Configure mimalloc
+echo '——Configure mimalloc——'
+CC=clang CFLAGS='-O3 -flto=thin' CXX=clang++ CXXFLAGS=$CFLAGS LDFLAGS='-fuse-ld=lld' cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/opt/musical-octo-fortnight .
+
+#Build mimalloc
+cmake --build . --parallel $(nproc)
+
+#Install mimalloc   
+cmake --install .
