14fa89d143c17e769e95c3349f14a964567a8ab3
compact-orb <89509108+compact-orb@users.noreply.github.com>
2024-12-02 04:10:03 +0000

    Wait 16 seconds before test

--- bind9.sh
+++ bind9.sh
@@ -286,7 +286,7 @@ zone rpz.oisd.nl. {
 echo '——Run test——'
 /opt/musical-octo-fortnight/usr/sbin/named -f &
 NAMED_PID=$!
-sleep 2
+sleep 16
 dns_lookup() {
 	/opt/musical-octo-fortnight/usr/bin/dig @::1 a $1 "$domain"
 	/opt/musical-octo-fortnight/usr/bin/dig @::1 aaaa $1 "$domain"
