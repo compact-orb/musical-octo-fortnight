0a0ce5e9743a556830ff7ba867d36fc957418e7e
compact-orb <compact-orb@outlook.com>
2024-12-08 11:07:34 +0000

    Add server authentication certicate generation script

---
+++ compact-orb-server-authentication.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/bash
+
+#Modified unofficial Bash strict mode
+set -eo pipefail
+IFS=$'\n\t'
+
+#Use global profile
+. /usr/share/defaults/etc/profile
+
+#Save repository directory
+REPOSITORY_DIR=$(pwd)
+
+#Generate in /tmp
+echo '——Build in /tmp——'
+BUILD_DIR=/tmp
+cd $BUILD_DIR
+
+#Install required dependencies
+echo '——Install required dependencies——'
+pip install --user certbot
+sudo ln --symbolic /home/codespace/.local/bin/certbot /usr/local/bin/certbot
+
+#Generate certificates
+echo '——Generate certificates——'
+function generate-certificate {
+	sudo certbot certonly --agree-tos -m compact-orb@compact-orb.ovh --eab-kid $EAB_KID --eab-hmac-key $EAB_HMAC_KEY --preferred-challenges dns-01 --server https://acme.zerossl.com/v2/DV90 --csr "$1.csr" --no-eff-email --manual
+	awk '/-----BEGIN CERTIFICATE-----/ {if (NR > 1) print cert; cert = $0; next} {cert = cert "\n" $0}' 0001_chain.pem > "$REPOSITORY_DIR/server-authentication/$1.crt"
+	sudo rm 000*.pem
+}
+generate-certificate "compact-orb.ovh"
+generate-certificate "compact-orb.ovh-rsa"
+generate-certificate "*.compact-orb.ovh"
+generate-certificate "*.compact-orb.ovh-rsa"
+echo -e $COMPACT_ORB_KEY > $REPOSITORY_DIR/server-authentication/compact-orb.key
+echo -e $COMPACT_ORB_RSA_KEY > $REPOSITORY_DIR/server-authentication/compact-orb-rsa.key
